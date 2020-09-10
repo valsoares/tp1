cf7cb3926ed1138576e434d8941504fca00bc9f2
Bitt <bittenmat@gmail.com>
2020-09-10 19:23:22 -0300

    Todas as classes criadas

--- lib/dominios.h
+++ lib/dominios.h
@@ -5,6 +5,134 @@
 
 using namespace std;
 
+class Cep {
+    private:
+        int cep;
+
+        bool validaCep(int cep);
+    
+    public:
+        void setCep(int cep);
+
+        int getCep() {
+            return cep;
+        }
+};
+
+class Classe {
+    private:
+        string classe;
+
+        bool validaClasse(string classe);
+    
+    public:
+        void setClasse(string classe);
+
+        string getClasse() {
+            return classe;
+        }
+};
+
+class CodigoAgencia {
+    private:
+        int codigo;
+
+        bool validaCodigo(int codigo);
+    
+    public:
+        void setCodigo(int codigo);
+
+        int getCodigo() {
+            return codigo;
+        }
+};
+
+class CodigoAplicacao {
+    private:
+        int codigo;
+
+        bool validaCodigo(int codigo);
+    
+    public:
+        void setCodigo(int codigo);
+
+        int getCodigo() {
+            return codigo;
+        }
+};
+
+class CodigoBanco {
+    private:
+        int codigo;
+
+        bool validaCodigo(int codigo);
+    
+    public:
+        void setCodigo(int codigo);
+
+        int getCodigo() {
+            return codigo;
+        }
+};
+
+class CodigoProduto {
+    private:
+        int codigo;
+
+        bool validaCodigo(int codigo);
+    
+    public:
+        void setCodigo(int codigo);
+
+        int getCodigo() {
+            return codigo;
+        }
+};
+
+class Cpf {
+    private:
+        int cpf;
+
+        bool validaCpf(int cpf);
+    
+    public:
+        void setCpf(int cpf);
+
+        int getCpf() {
+            return cpf;
+        }
+};
+
+class Data {
+   private: 
+        string data;
+
+        bool validaData(string data);
+
+    public:
+        void setData(string data);
+
+        string getData() {
+            return data;
+        }
+
+};
+
+class Emissor {
+   private: 
+        string emissor;
+
+        bool validaEmissor(string emissor);
+
+    public:
+        void setEmissor(string emissor);
+
+        string getEmissor() {
+            return emissor;
+        }
+
+};
+
 class Endereco {
     private: 
         string texto;
