0b624001a4fe0d3559930e9ea38202c2dc61c6b6
valsoares <valescasoares@gmail.com>
2020-10-12 19:06:18 -0300

    adicionando arquivos do trabalho 2

---
+++ trabalho2/include/entidades.h
@@ -0,0 +1,16 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+
+
+class entidades
+{
+    public:
+        entidades();
+        virtual ~entidades();
+
+    protected:
+
+    private:
+};
+
+#endif // ENTIDADES_H
